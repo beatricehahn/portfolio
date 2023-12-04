c4594c9904079986b04d9ffac27bdab7f5ac76db
Beatrice Hahn <beatricehahn7@gmail.com>
2023-12-04 15:36:49 -0800

    Setup skeleton layout for about and work sections

--- my-portfolio/src/routes/work/+page.svelte
+++ my-portfolio/src/routes/work/+page.svelte
@@ -1,12 +1,9 @@
-<h1>This is the homepage</h1>
+<h3>All Projects</h3>
 
-<h2>[Add my quote here later]</h2>
-
-<h3>Recent Work</h3>
 <ul>
     <li><a href="/work/one">Project 1 Title</a></li>
     <li><a href="/work/two">Project 2 Title</a></li>
     <li><a href="/work/three">Project 3 Title</a></li>
+    <li><a href="/work/four">Project 4 Title</a></li>
+    <li><a href="/work/five">Project 5 Title</a></li>
 </ul>
-
-<a href="/work">View All</a>
