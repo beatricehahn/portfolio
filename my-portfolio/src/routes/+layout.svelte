5104eb603eb6cefb9160b8cea527695da9332f09
Beatrice Hahn <beatricehahn7@gmail.com>
2023-12-11 17:06:25 -0800

    Set placeholder layout styles and content

--- my-portfolio/src/routes/+layout.svelte
+++ my-portfolio/src/routes/+layout.svelte
@@ -1,7 +1,98 @@
+<script>
+    import './global.css';
+</script>
+
 <nav>
-    <a href='/'>Home</a>
-    <a href='/about'>About</a>
-    <a href='/work'>Work</a>
+    <div class="logo-wrapper">
+        <div class="logo"></div>
+        <span>BEATRICE HAHN</span>
+    </div>
+    <div>
+        <ul class='nav-links'>
+            <li><a href='#about-section' class='nav-link'>ABOUT</a></li>
+            <li><a href='#work-section' class='nav-link'>WORK</a></li>
+            <li><a href='#about-section' class='nav-link'>RESUME</a></li>
+            <li><a href='#contact-section' class='nav-link'>CONTACT</a></li>
+        </ul>
+    </div>
 </nav>
 
-<slot />
+<main>
+    <slot></slot>
+</main>
+
+<footer>
+    <div id='footer-nav'>
+        <ul>
+            <span>MENU</span>
+            <div class='category-divider'></div>
+            <li><a href="#about-section">ABOUT</a></li>
+            <li><a href="#work-section">WORK</a></li>
+            <li><a href="#about-section">RESUME</a></li>
+            <li><a href="#contact-section">CONTACT</a></li>
+        </ul>
+    </div>
+    <div id='footer-contacts'>
+        <ul>
+            <span>UPDATES</span>
+            <div class='category-divider'></div>
+            <li><a href="linkedin.com/in/beatrice-hahn">LinkedIn</a></li>
+            <li><a href='github'>Github</a></li>
+            <li><a href='itch.io'>Itch.io</a></li>
+        </ul>
+    </div>
+    <span>Beatrice Hahn 2024</span>
+</footer>
+
+<style>
+    nav {
+        align-items: center;
+        background-color: rgba(255, 255, 255, 0.8);
+        backdrop-filter: blur(5px);
+        display: flex;
+        justify-content: space-between;
+        position: sticky;
+        top: 0;
+        z-index: 100;
+    }
+
+    .nav-links {
+        display: flex;
+    }
+
+    .nav-links > li > a {
+        margin-left: 20px;
+        margin-right: 20px;
+    }
+    .logo-wrapper {
+        display: flex;
+        align-items: center;
+
+    }
+    .logo {
+        background-color: var(--black-800);
+        height: 20px;
+        margin-right: 20px;
+        width: 20px;
+    }
+
+    li {
+        list-style-type: none;
+    }
+
+    nav, footer {
+        padding-top: 5vh;
+        padding-left: 96px;
+        padding-right: 96px;
+    }
+
+    footer {
+        display: flex;
+        justify-content: space-between;
+        padding-bottom: 16px;
+    }
+
+    footer > span {
+        font-size: 10px;
+    }
+</style>
--- my-portfolio/src/routes/+page.svelte
+++ my-portfolio/src/routes/+page.svelte
@@ -1,12 +1,151 @@
-<h1>This is the homepage</h1>
+<section id='hero-section'>
+    <h1>This is the homepage</h1>
+    <p id='hero-summary'>This is the hero section text. Lorem Ipsum.
+        This is the hero section text. Lorem Ipsum.
+        This is the hero section text. Lorem Ipsum.
+        This is the hero section text. Lorem Ipsum.
+    </p>
+</section>
 
-<h2>[Add my quote here later]</h2>
+<section id='quote-section'>
+    <quote>[Add my quote here later]</quote>
+</section>
 
-<h3>Recent Work</h3>
-<ul>
-    <li><a href="/work/one">Project 1 Title</a></li>
-    <li><a href="/work/two">Project 2 Title</a></li>
-    <li><a href="/work/three">Project 3 Title</a></li>
-</ul>
+<section id='about-section'>
+    <h2>ABOUT</h2>
+    <p>This is description of myself. Placeholder for description.This is description of myself. Placeholder for description. </p>
+    <a href='#' class='primary-btn'>Download Resume</a>
+</section>
 
-<a href="/work">View All</a>
+<section id='work-section'>
+    <h2>SELECTED WORK</h2>
+    <ul>
+        <li>
+            <div class='project-wrapper'>
+                <div class='project-info'>
+                    <h6>CATEGORY</h6>
+                    <div class='category-divider'></div>
+                    <h4>PROJECT TITLE</h4>
+                    <p>Short description of what the project is.</p>
+                    <a href="#" class='primary-btn'>View Case Study</a>
+                </div>
+                <div class='project-img-placeholder'>
+                    <img src='' alt='project splash screen'>
+                </div>
+            </div>
+        </li>
+        
+        <li>
+            <div class='project-wrapper'>
+                <div class='project-info'>
+                    <h6>CATEGORY</h6>
+                    <div class='category-divider'></div>
+                    <h4>PROJECT TITLE</h4>
+                    <p>Short description of what the project is.</p>
+                    <a href="#" class='primary-btn'>View Case Study</a>
+                </div>
+                <div class='project-img-placeholder'>
+                    <img src='' alt='project splash screen'>
+                </div>
+            </div>
+        </li>
+    
+        <li>
+            <div class='project-wrapper'>
+                <div class='project-info'>
+                    <h6>CATEGORY</h6>
+                    <div class='category-divider'></div>
+                    <h4>PROJECT TITLE</h4>
+                    <p>Short description of what the project is.</p>
+                    <a href="#" class='primary-btn'>View Case Study</a>
+                </div>
+                <div class='project-img-placeholder'>
+                    <img src='' alt='project splash screen'>
+                </div>
+            </div>
+        </li>
+    </ul>
+</section>
+
+<section id='process-section'>
+    <h2>DESIGN AND DEVELOPMENT PROCESS</h2>
+    <p>Explanation of my thinking process</p>
+    <br>
+    <h2>SKILLS</h2>
+    <br>
+    <h2>TOOLS</h2>
+</section>
+
+<section id='contact-section'>
+    <h2>CONTACT</h2>
+    <div class='email'></div>
+    <div class="linkedin"></div>
+</section>
+
+<style>
+    * {
+        width: 100%vw;
+        margin: 0;
+    }
+    h1, h2, h3, h4, h5, h6 {
+        font-family: 'BebasNeue-Regular', sans-serif;
+    }
+    #hero-section {
+        height: 90vh;
+        background-color: rgb(203, 203, 103);
+    }
+
+    #hero-summary {
+        width: 50vw;
+    }
+
+    #quote-section {
+        background-color: var(--black-800);
+        color: var(--off-white);
+        height: 10vh;
+    }
+
+    #about-section {
+        background-color: rgb(216, 129, 129);
+    }
+
+    #work-section {
+        background-color: rgb(33, 155, 155);
+    }
+
+    .project-wrapper {
+        display: flex;
+        padding-top: 64px;
+        padding-bottom: 32px;
+    }
+
+    .project-info > p {
+        margin-top: 24px;
+        margin-bottom: 24px;
+        padding-right: 128px;
+    }
+
+    .project-img-placeholder{
+        background-color: rgb(186, 134, 235);
+        height: 250px;
+        width: 400px;
+    }
+
+    #process-section {
+        background-color: beige;
+    }
+
+    #contact-section {
+        background-color: var(--black-800);
+        color: var(--off-white);
+        height: 10vh;
+    }
+
+    ul {
+        padding: 0
+    }
+    li {
+        list-style-type: none;
+    }
+
+</style>
