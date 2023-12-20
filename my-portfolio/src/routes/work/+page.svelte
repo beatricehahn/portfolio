d4b0d94bb173dcaff4923524916fd7d31329bd68
Beatrice Hahn <beatricehahn7@gmail.com>
2023-12-20 14:25:36 -0800

    Use work component as children

--- my-portfolio/src/routes/work/+page.svelte
+++ my-portfolio/src/routes/work/+page.svelte
@@ -1,9 +1,13 @@
+<script>
+    import Work from '../../components/atoms/work.svelte';
+</script>
+
 <h3>All Projects</h3>
 
 <ul>
-    <li><a href="/work/one">Project 1 Title</a></li>
-    <li><a href="/work/two">Project 2 Title</a></li>
-    <li><a href="/work/three">Project 3 Title</a></li>
-    <li><a href="/work/four">Project 4 Title</a></li>
-    <li><a href="/work/five">Project 5 Title</a></li>
+    <li><a href="/work/one"><Work /></a></li>
+    <li><a href="/work/two"><Work /></a></li>
+    <li><a href="/work/three"><Work /></a></li>
+    <li><a href="/work/four"><Work /></a></li>
+    <li><a href="/work/five"><Work /></a></li>
 </ul>
