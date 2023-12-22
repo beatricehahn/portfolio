7105d3741f87ba99152b8f457961bd13ecdadd6a
Beatrice Hahn <beatricehahn7@gmail.com>
2023-12-21 21:49:34 -0800

    Revert "Merge branch 'main' of https://github.com/beatricehahn/portfolio"
    This reverts commit 78fa4efba357ddea504447abd9710365f506d44c, reversing
    changes made to 4804c325b9e73d6ad6765b6d8aa87598a9ef8bdd.

--- my-portfolio/src/components/atoms/menu.svelte
+++ my-portfolio/src/components/atoms/menu.svelte
@@ -4,98 +4,60 @@
     // toggles isMenuOpen value
     const handleMenuToggle = () => {
         isMenuOpen = !isMenuOpen;
-
-        if (isMenuOpen) {
-            document.getElementById('menu').style.height = '100%';
-        }
-        else {
-            document.getElementById('menu').style.height = '0%';
-        }
     }
 
     // set menu to closed if focus is lost
-    // const handleFocusLoss = ({relatedTarget, currentTarget}) => {
-    //     if (relatedTarget instanceof HTMLElement && currentTarget.contains(relatedTarget)) return;
-    //     isMenuOpen = false;
-    // }
-
-    // when user navigates to a different page, close overlay
-    const navigate = () => {
+    const handleFocusLoss = ({relatedTarget, currentTarget}) => {
+        if (relatedTarget instanceof HTMLElement && currentTarget.contains(relatedTarget)) return;
         isMenuOpen = false;
-        document.getElementById('menu').style.height = '0%';
     }
-
 </script>
 
-<div id='menu' class='overlay'>
-    <!-- Overlay content-->
-    <ul class='overlay-content'>
-        <li><a href="/work" class='menu-link' on:click={navigate}>WORK</a></li>
-        <li><a href="/about" class='menu-link'>ABOUT</a></li>
-        <li><a href="/workflow" class='menu-link'>SERVICES</a></li>
-        <li><a href="/contact" class='menu-link'>CONTACT</a></li>
-    </ul>
-</div>
+<div id='menu' on:focusout={handleFocusLoss}>
+    <div class='dropdown'>
+        
+        <button on:click={handleMenuToggle}>
+        {#if isMenuOpen}
+            <p>CLOSE</p>
+    
+        {:else}
+            <p>MENU</p>
+        {/if}
+        </button>
 
-<button class='menu-btn' on:click={handleMenuToggle}>MENU</button>
+        <ul class='dropdown-content' style:visibility={isMenuOpen ? 'visible' : 'hidden'}>
+            <li><a href="/work">WORK</a></li>
+            <li><a href="/about">ABOUT</a></li>
+            <li><a href="/workflow">SERVICES AND WORKFLOW</a></li>
+            <li><a href="/contact">CONTACT</a></li>
+        </ul>
+    </div>
+</div>
 
 <style lang='scss'>
     @import '../../styles/globalVar.scss';
-    @import '../../styles/fonts.css';
-
-    .menu-btn {
-        background-color: $ui-btn-grey;
-        border-radius: 4px;
-        padding-left: 8px;
-        padding-right: 8px;
-        font-weight: bold;
-        height: 32px;
-        width: 48px;
-        z-index: 20;
-    }
-
+    
     li {
         list-style-type: none;
     }
 
-    .overlay {
-        height: 0;
-        width: 100%;
-        position: fixed;
-        z-index: 20;
-        left: 0;
-        top: 0;
-        background-color: rgba(0, 0, 0, 0.9);
-        overflow-x: hidden; /* Prevent horizontal scroll */
-        transition: 0.5s;
-    }
+    .dropdown {
+        float: left;
+        overflow: hidden;
 
-    .overlay-content {
-        position: relative;
-        top: 25%;
-        width: 100%;
-        text-align: center;
-        margin-top: 30px;
-    }
+        button {
+            background-color: $ui-btn-grey;
+            border-radius: 4px;
+            padding-left: 8px;
+            padding-right: 8px;
+            font-weight: bold;
+        }
 
-    .menu-link {
-        padding: 8px;
-        text-decoration: none;
-        font-family: 'BebasNeue-Regular';
-        font-size: 48px;
-        color: $off-white;
-        transition: 0.3s;
-    }
-
-    .menu-link:hover, .menu-link:focus {
-        color: $royal-purple-highlight;
-    }
-
-    @media screen and (max-height: 450px) {
-        .menu-link {
-            font-size: 32px;
+        .dropdown-content {
+            position: absolute;
+            background-color: orange;
+            z-index: 2;
         }
     }
-
 
 </style>
